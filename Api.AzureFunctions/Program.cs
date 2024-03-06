2358224ab58bfbc2493c788eeeee618183066202
Corentin Piccioli <corentin.piccioli@diiage.org>
2024-03-06 15:28:38 +0100

    fix: Connexion to api

--- Api.AzureFunctions/Program.cs
+++ Api.AzureFunctions/Program.cs
@@ -1,7 +1,32 @@
+using Api.DbContext;
+using Api.Repository;
+using Api.Repository.Contracts;
+using Api.Services;
+using Api.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .Build();
 
-host.Run();
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureServices((hostBuilder, services) =>
+            {
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(hostBuilder.Configuration.GetConnectionString("MyConnectionString")));
+                
+                services.AddScoped<IEventService, EventService>();
+                services.AddScoped<IEventRepository, EventRepository>();
+                services.AddLogging();
+
+            })
+            .Build();
+        host.Run();
+    }
+}
+
